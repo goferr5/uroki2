4b240a34e0723e465a1052e379705dbca41dd462
Alex <goferr5@gmail.com>
2018-07-04 20:17:08 +0300

    второй комит

---
+++ lesson 1/script.js
@@ -0,0 +1,49 @@
+//var first = 1;
+//
+//var LeftBorderWidth =1;
+//
+//
+//const pi = 3.14;
+//
+//{
+//	let second = 2; 
+//}
+//
+////console.log(4/0);
+////
+////console.log('string' * 9);
+//
+//var und
+//console.log(und);
+//
+//obj = {
+//	name: "John",
+//	age: 25,
+//	isMarried: false
+//}
+//
+//console.log(obj["name"]);
+//
+//var arr = ['plum', 'orange', 'apple'];
+//console.log(arr[0], arr[2]);
+//
+////alert('Hello');
+////confirm('Are you hear?');
+//
+////var answer = prompt("Вам есть 18?", "18");
+//console.log( typeof(arr));
+
+var budzhet = prompt("Ваш бюджет?", "100 грн");
+var nameShop = prompt("Название вашего магазина?");
+
+var mainList = {
+	budzhet: 10000,
+	nameShop: "Rognezia",
+	var shopGoods = ['1','2','3','4'];
+	employers = {
+		name: "John",
+		age: 33,
+		isMarried: false
+	}
+	
+}
